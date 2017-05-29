5bfded15db8503238ad19ffc0ccb1cfaaa28e80c
Francky <testandro.hd@gmail.com>
2017-05-29 14:18:54 +0200

    1er commit

---
+++ src/main/java/co/simplon/CarnetDeBordSimplon/CarnetDeBordSimplonApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.CarnetDeBordSimplon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnetDeBordSimplonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnetDeBordSimplonApplication.class, args);
+	}
+}
