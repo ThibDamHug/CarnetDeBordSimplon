f751f006b82b1699821387b6b5abac7652452457
damien <damienpradier@msn.com>
2017-06-14 14:45:37 +0200

    maj de spring security

--- src/main/java/co/simplon/security/WebSecurityConfig.java
+++ src/main/java/co/simplon/security/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import co.simplon.security.rest.configuration.RESTAuthenticationEntryPoint;
 import co.simplon.security.rest.configuration.RESTAuthenticationFailureHandler;
@@ -22,26 +21,24 @@ import co.simplon.security.rest.configuration.RESTLogoutSuccessHandler;
 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	@Autowired
-	
+	@Autowired	
 	RESTAuthenticationEntryPoint authenticationEntryPoint;
 	
 	@Autowired
-	
 	RESTAuthenticationSuccessHandler authenticationsuccessHandler;
 	
 	@Autowired
-	
 	RESTAuthenticationFailureHandler authenticationFailureHandler;
 	
-	@Autowired
-	
+	@Autowired	
 	RESTLogoutSuccessHandler logoutSuccessHandler;
 	
 	//Override la methode configure
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.httpBasic()
+	  	.and()	
         	.exceptionHandling()
   				.authenticationEntryPoint(authenticationEntryPoint)
   		.and()
@@ -49,20 +46,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   				.usernameParameter("email")
   				.successHandler(authenticationsuccessHandler)
   				.failureHandler(authenticationFailureHandler)
-  				.permitAll()
   		.and()
   			.authorizeRequests()
-		//.antMatchers("/users/**").hasAnyAuthority("ROLE_administrateur","ROLE_apprenant","Role_caca")
-		//.antMatchers("/roles/**").authenticated()
-  			.anyRequest().authenticated()
+  			.antMatchers("/api/**").authenticated()
   		.and()
-  			.httpBasic()
-  		.and()
-  		.logout()
-  			.logoutSuccessHandler(logoutSuccessHandler)
-  			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-  			.deleteCookies("JSESSIONID")
-  			.permitAll()
+	  		.logout()
+	  			.logoutSuccessHandler(logoutSuccessHandler)
   		.and()
   			.csrf()
   			.disable();
