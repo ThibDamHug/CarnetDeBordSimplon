dcc444b24ef9338dd65c3dc506a7f36886f12c76
Abdel <ahmar.abdel@gmail.com>
2017-06-19 14:54:52 +0200

    Implementation Logger + Ajout Captures

--- src/main/java/co/simplon/constantes/LogConstants.java
+++ src/main/java/co/simplon/constantes/LogConstants.java
@@ -1,9 +1,10 @@
 package co.simplon.constantes;
 
 public class LogConstants {
-
-	 public final static String ADVICE_SERVICE = "Advice Controller";
-	 
-	 public final static String AUTHENTIFICATION_SERVICE = "Service d'authentification";
-	 
+	/**
+	 * MainRestControllerAdvice
+	 */
+	public static final String ADVICE_CONTROLLER = "MainRestControllerAdvice";
+	
+	public static final String AUTHENTIFICATION_SERVICE = "Service d'authentification";	 
 }
--- src/main/java/co/simplon/restcontrolleradvice/MainRestControllerAdvice.java
+++ src/main/java/co/simplon/restcontrolleradvice/MainRestControllerAdvice.java
@@ -1,6 +1,8 @@
 package co.simplon.restcontrolleradvice;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import co.simplon.constantes.ErrorMessageEnum;
 import co.simplon.constantes.LogConstants;
 import co.simplon.exceptions.CustomException;
-import co.simplon.exceptions.CustomLogger;
 import co.simplon.models.ExceptionMessage;
 /**
  * 
@@ -18,22 +19,21 @@ import co.simplon.models.ExceptionMessage;
  *
  */
 @RestControllerAdvice
-public class MainControllerAdvice {
-	
-	@Autowired
-	CustomLogger log;
+public class MainRestControllerAdvice {
+
+	private static Logger logger = Logger.getLogger(LogConstants.ADVICE_CONTROLLER);
 	
 	@ExceptionHandler(CustomException.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ExceptionMessage handleCustomException(CustomException exception) {
-		log.authLog(LogConstants.ADVICE_SERVICE, new Object().getClass().getEnclosingMethod().getName());
+		logger.log(Level.SEVERE, exception.getMessage());
 		return new ExceptionMessage(ErrorMessageEnum.getByCode(exception.getErrorCode()));	
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ExceptionMessage handleValidation (MethodArgumentNotValidException exception) {
-		log.authLog(LogConstants.ADVICE_SERVICE, new Object().getClass().getEnclosingMethod().getName());
+		logger.log(Level.SEVERE, exception.getMessage());
 		return new ExceptionMessage(ErrorMessageEnum.VALIDATION);
 	}
 }
