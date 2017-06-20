6bcff5d1146d66fca946f0ff165e3618f8d03660
Francky <testandro.hd@gmail.com>
2017-06-20 12:00:48 +0200

    Mis a jour de AnswerController

--- src/main/java/co/simplon/controllers/AnswerRestController.java
+++ src/main/java/co/simplon/controllers/AnswerRestController.java
@@ -3,6 +3,7 @@ package co.simplon.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.simplon.constantes.ErrorMessageEnum;
+import co.simplon.constantes.SecurityRoleConstants;
 import co.simplon.constantes.UriConstants;
 import co.simplon.exceptions.CustomException;
 import co.simplon.models.Answer;
@@ -28,12 +30,15 @@ public class AnswerRestController {
 	@Autowired
 	AnswerService service;
 	
+	@PreAuthorize(SecurityRoleConstants.TEACHER)
 	@GetMapping
 	public List<Answer> getByDiaryAndStudent(@RequestParam int diaryId,
 											@RequestParam int studentId) {
 		return service.getByDiaryAndStudent(diaryId, studentId);
 	}
 	
+	@PreAuthorize(SecurityRoleConstants.STUDENT + " or " +
+	  		SecurityRoleConstants.TUTOR)  
 	@PostMapping
 	public List<Answer> saveAnswers(@Validated @RequestBody List<Answer> answers) {
 		List<Answer> result = service.saveAll(answers);
